d9155c89ca5be4ed7c06c09a1f7a7bbfc03ea75c
Farheen-13 <farheenshaikfarru854@gmail.com>
2025-06-30 15:28:07 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -11,19 +11,36 @@ public class Employee {
         this.salary = salary;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
+    public String getDepartment() {
+        return department;
+    }
 
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
-        return "ID: " + id + ", Name: " + name + ", Department: " + department + ", Salary: " + salary;
+        return "Employee [ID=" + id + ", Name=" + name + ", Department=" + department + ", Salary=" + salary + "]";
     }
 }
